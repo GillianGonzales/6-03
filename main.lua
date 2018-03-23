e88fefa3d5c103f58b4688ef6737f65dee003ddd
Gillian <gillian.gonzales@ocsbstudent.ca>
2018-03-23 13:19:38 -0400

    ageMovie
    This will tell the user of what movie that this person can watch

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@
 --
 -- main.lua
 --
+-- Created By Gillian Gonzales
+-- Created On March 23 2018
+--
+-- This program will tell the user what movie that he/she can watch based on their age 
 -----------------------------------------------------------------------------------------
 
 -- Making Varabiles
@@ -10,7 +14,7 @@ local age
  -- Changing Background
  display.setDefault("background", 1, 1, 1 )
 
--- Adding Text]
+-- Adding Text
 local instructions = display.newText("Type In your age", display.contentCenterX, display.contentCenterY - 200, native.systemFont, 100 )
 instructions:setFillColor (0,0,0)
 
@@ -25,13 +29,14 @@ local button = display.newImageRect("./assets/sprites/Button.png",406,157)
 button.x = display.contentCenterX
 button.y = display.contentCenterY + 200
 
+--Making Function
 local function tellMovie ( event )
 	-- This function will tell what movie that the user can watch 
 
 	-- Assgining varibles
 	age = tonumber(userInputField.text) 
 	
-
+		-- Making if statment
 		if age > 17 then
 			whatMovie.text = "You can watch a R rated movie"
 		elseif age > 13 then
